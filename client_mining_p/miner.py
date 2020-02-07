f2d843139bd09f935f52fefce9a9b1f75f2eb590
ykrueng <yasirah.krueng@gmail.com>
2020-02-06 19:40:30 -0600

    finish basic transactions

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -37,7 +37,7 @@ def valid_proof(block_string, proof):
     guess = f'{block_string}{proof}'.encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
     # print(guess_hash)
-    return guess_hash[:6] == '000000'
+    return guess_hash[:5] == '00000'
 
 
 if __name__ == '__main__':
